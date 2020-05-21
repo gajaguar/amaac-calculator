426c715e4deb0971a188f941de89d7649f0b4fd2
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-21 16:42:57 -0500

    Prepare routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Generators from '@/views/generators/Index.vue'
+import Amaac from '@/views/generators/amaac/Index.vue'
 
 Vue.use(VueRouter)
 
@@ -10,14 +12,24 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/generators',
+    name: 'generators',
+    component: Generators
+  },
+  {
+    path: '/generators/amaac',
+    name: 'generators.amaac',
+    component: Amaac
   }
 ]
 
