e23032d5728d6ec6e03ba1d4bba3b7f14619c080
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-21 16:41:11 -0500

    Import only needed Bulma components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import styles from '@/styles/main.sass'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  styles,
   render: (h) => h(App)
 }).$mount('#app')
