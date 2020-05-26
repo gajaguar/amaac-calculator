32b7aeae00caab5b2f5d0dbcef955178a2785bab
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-26 17:35:35 -0500

    Refactor the Asphalt Content component

--- src/views/calculators/amaac/AsphaltContent.vue
+++ src/views/calculators/amaac/AsphaltContent.vue
@@ -3,52 +3,35 @@
     <p class="title">Contenido de Asfalto en la Mezcla (P<sub>b</sub>)</p>
     <form>
       <!-- data -->
-      <app-field :label="labels[0]" step="0.1" v-model="wp" />
-      <app-field :label="labels[1]" step="0.1" v-model="wbi" />
-      <app-field :label="labels[2]" step="0.1" v-model="wbf" />
+      <app-field
+        @input="calculateAll"
+        :key="f"
+        :label="field.label"
+        step="0.1"
+        v-for="(field, f) in fields"
+        v-model="field.data"
+      />
 
       <!-- results -->
       <app-field
-        color="is-primary"
-        :label="labels[3]"
+        :key="r"
+        :label="result.label"
         :readonly="true"
-        :value="wni"
-      />
-      <app-field
+        :value="result.data"
         color="is-primary"
-        :label="labels[4]"
-        :readonly="true"
-        :value="wnf"
-      />
-      <app-field
-        color="is-primary"
-        :label="labels[5]"
-        :readonly="true"
-        :value="wa"
-      />
-      <app-field
-        color="is-primary"
-        :label="labels[6]"
-        :readonly="true"
-        :value="pb_"
-      />
-      <app-field
-        color="is-primary"
-        :label="labels[7]"
-        :readonly="true"
-        :value="pb"
-      />
-      <app-field
-        color="is-primary"
-        :label="labels[8]"
-        :readonly="true"
-        :value="ps"
+        v-for="(result, r) in results"
       />
     </form>
   </div>
 </template>
 <script>
 import AppField from '@/components/AppField.vue'
+import {
+  aggregatesByMixture,
+  asphaltByAggregates,
+  asphaltByMixture
+} from '@/utils/contents'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   name: 'AsphaltContent',
@@ -56,69 +39,73 @@ export default {
   components: { AppField },
 
   data: () => ({
-    labels: [
-      'Masa del papel filtro (g)',
-      'Masa bruta previa al lavado (g)',
-      'Masa bruta posterior al lavado (g)',
-      'Masa neta previa al lavado (g)',
-      'Masa neta posterior al lavado (g)',
-      'Masa del asfalto lavado (g)',
-      "Contenido de asfalto por agregado seco (%) [P _b_ ']",
-      'Contenido de asfalto en la mezcla (%) [P _b_ ]',
-      'Contenido de agregado en la mezcla (%) [P _s_ ]'
+    aggregatesByMixture,
+    asphaltByAggregates,
+    asphaltByMixture,
+    fields: [
+      {
+        label: 'Masa del papel filtro (g)',
+        data: ''
+      },
+      {
+        label: 'Masa bruta previa al lavado (g)',
+        data: ''
+      },
+      {
+        label: 'Masa bruta posterior al lavado (g)',
+        data: ''
+      }
     ],
-    wbi: '0.0',
-    wbf: '0.0',
-    wp: '0.0'
+    results: [
+      {
+        label: "Contenido de asfalto por agregado seco (%) [P _b_ ']",
+        data: ''
+      },
+      {
+        label: 'Contenido de asfalto en la mezcla (%) [P _b_ ]',
+        data: ''
+      },
+      {
+        label: 'Contenido de agregado en la mezcla (%) [P _s_ ]',
+        data: ''
+      }
+    ]
   }),
 
   computed: {
-    pb_() {
-      // Pb' = (Wbi - Wbf) / (Wbf - Wp) * 100
-      const wbi = Number(this.wbi)
-      const wbf = Number(this.wbf)
-      const wp = Number(this.wp)
-      if (wbi === 0 || wbf === 0 || wp === 0) return '0.000'
-      const pb_ = ((wbi - wbf) / (wbf - wp)) * 100
-      return pb_.toFixed(3)
+    ...mapState('contents', ['pb', 'pb_', 'ps']),
+    wp() {
+      return this.fields[0].data
     },
-    pb() {
-      // Pb = Pb' / (100 + Pb') * 100
-      const pb_ = Number(this.pb_)
-      if (pb_ === 0) return '0.000'
-      const pb = (pb_ / (100 + pb_)) * 100
-      return pb.toFixed(3)
+    wbi() {
+      return this.fields[1].data
     },
-    ps() {
-      // Ps = 100 - Pb
-      const pb = Number(this.pb)
-      if (pb === 0) return '0.000'
-      const ps = 100 - pb
-      return ps.toFixed(3)
+    wbf() {
+      return this.fields[2].data
+    }
+  },
+
+  methods: {
+    ...mapMutations('contents', ['updatePb_', 'updatePb', 'updatePs']),
+    calculatePb() {
+      const pb = this.asphaltByMixture(this.pb_)
+      this.updatePb({ value: pb })
+      this.results[1].data = this.pb
     },
-    wa() {
-      // Wa = Wbi - Wbf
-      const wbi = Number(this.wbi)
-      const wbf = Number(this.wbf)
-      if (wbi === 0 || wbf === 0) return '0.000'
-      const wa = wbi - wbf
-      return wa.toFixed(3)
+    calculatePb_() {
+      const pb_ = this.asphaltByAggregates(this.wbi, this.wbf, this.wp)
+      this.updatePb_({ value: pb_ })
+      this.results[0].data = this.pb_
     },
-    wni() {
-      // Wni = Wbi - Wp
-      const wbi = Number(this.wbi)
-      const wp = Number(this.wp)
-      if (wbi === 0 || wp === 0) return '0.000'
-      const wni = wbi - wp
-      return wni.toFixed(3)
+    calculatePs() {
+      const ps = this.aggregatesByMixture(this.pb)
+      this.updatePs({ value: ps })
+      this.results[2].data = this.ps
     },
-    wnf() {
-      // Wnf = Wbf - Wp
-      const wbf = Number(this.wbf)
-      const wp = Number(this.wp)
-      if (wbf === 0 || wp === 0) return '0.000'
-      const wnf = wbf - wp
-      return wnf.toFixed(3)
+    calculateAll() {
+      this.calculatePb_()
+      this.calculatePb()
+      this.calculatePs()
     }
   }
 }
