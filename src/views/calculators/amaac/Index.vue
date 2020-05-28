db99e05611e73361618bfcbd4de9c5628373d631
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-28 10:24:57 -0500

    Change display options
    Calculator AMAAC view now show form organized in tabs
    and a right panel that display the results.

--- src/views/calculators/amaac/Index.vue
+++ src/views/calculators/amaac/Index.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <div class="columns">
-      <div class="column">
-        <div class="box"><asphalt-content /></div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <div class="tile is-child box">
+          <div class="tabs is-centered">
+            <ul>
+              <li
+                :class="{ 'is-active': tab.active }"
+                :key="t"
+                v-for="(tab, t) in tabs"
+              >
+                <a @click="toggler(t)">
+                  {{ tab.caption }}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <asphalt-content v-show="tabs[0].active" />
+          <aggregates-bulk-gravity v-show="tabs[1].active" />
+          <asphalt-gravity v-show="tabs[2].active" />
+          <mixture-bulk-gravity v-show="tabs[3].active" />
+        </div>
       </div>
-      <div class="column">
-        <div class="box"><aggregates-bulk-gravity /></div>
-      </div>
-      <div class="column">
-        <div class="box"><asphalt-gravity /></div>
-      </div>
-    </div>
-    <div class="columns">
-      <div class="column">
-        <div class="box"><mixture-bulk-gravity /></div>
+      <div class="tile is-parent is-4">
+        <div class="tile is-child box">
+          <results />
+        </div>
       </div>
     </div>
   </div>
@@ -24,6 +36,7 @@ import AggregatesBulkGravity from '@/views/calculators/amaac/AggregatesBulkGravi
 import AsphaltContent from '@/views/calculators/amaac/AsphaltContent.vue'
 import AsphaltGravity from '@/views/calculators/amaac/AsphaltGravity.vue'
 import MixtureBulkGravity from '@/views/calculators/amaac/MixtureBulkGravity.vue'
+import Results from '@/views/calculators/amaac/Results.vue'
 
 export default {
   name: 'Index',
@@ -32,7 +45,38 @@ export default {
     AggregatesBulkGravity,
     AsphaltContent,
     AsphaltGravity,
-    MixtureBulkGravity
+    MixtureBulkGravity,
+    Results
+  },
+
+  data: () => ({
+    tabs: [
+      {
+        caption: 'Contenidos',
+        active: true
+      },
+      {
+        caption: 'Agregados',
+        active: false
+      },
+      {
+        caption: 'Asflato',
+        active: false
+      },
+      {
+        caption: 'Mezcla',
+        active: false
+      }
+    ]
+  }),
+
+  methods: {
+    toggler(index) {
+      for (let i in this.tabs) {
+        this.tabs[i].active = false
+      }
+      this.tabs[index].active = true
+    }
   }
 }
 </script>
