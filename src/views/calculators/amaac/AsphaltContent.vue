c86eb37553ffa601731858609357072a1ab124b6
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-28 10:29:26 -0500

    Asphalt Content component now only get data
    Remove 'results' and auto generated fields.

--- src/views/calculators/amaac/AsphaltContent.vue
+++ src/views/calculators/amaac/AsphaltContent.vue
@@ -1,37 +1,38 @@
 <template>
   <div>
-    <p class="title">Contenido de Asfalto en la Mezcla (P<sub>b</sub>)</p>
+    <p class="title">
+      Contenido de Asfalto en la Mezcla (P<sub>b</sub>)
+    </p>
     <form>
-      <!-- data -->
       <app-field
         @input="calculateAll"
-        :key="f"
-        :label="field.label"
+        label="Masa del papel filtro (g)"
         step="0.1"
-        v-for="(field, f) in fields"
-        v-model="field.data"
+        v-model="wp"
       />
-
-      <!-- results -->
       <app-field
-        :key="r"
-        :label="result.label"
-        :readonly="true"
-        :value="result.data"
-        color="is-primary"
-        v-for="(result, r) in results"
+        @input="calculateAll"
+        label="Masa bruta previa al lavado (g)"
+        step="0.1"
+        v-model="wbi"
+      />
+      <app-field
+        @input="calculateAll"
+        label="Masa bruta posterior al lavado (g)"
+        step="0.1"
+        v-model="wbf"
       />
     </form>
   </div>
 </template>
 <script>
 import AppField from '@/components/AppField.vue'
+import { mapGetters, mapMutations } from 'vuex'
 import {
   aggregatesByMixture,
   asphaltByAggregates,
   asphaltByMixture
-} from '@/utils/contents'
-import { mapState, mapMutations } from 'vuex'
+} from '@/utils/amaac'
 
 export default {
   name: 'AsphaltContent',
@@ -42,65 +43,28 @@ export default {
     aggregatesByMixture,
     asphaltByAggregates,
     asphaltByMixture,
-    fields: [
-      {
-        label: 'Masa del papel filtro (g)',
-        data: ''
-      },
-      {
-        label: 'Masa bruta previa al lavado (g)',
-        data: ''
-      },
-      {
-        label: 'Masa bruta posterior al lavado (g)',
-        data: ''
-      }
-    ],
-    results: [
-      {
-        label: "Contenido de asfalto por agregado seco (%) [P _b_ ']",
-        data: ''
-      },
-      {
-        label: 'Contenido de asfalto en la mezcla (%) [P _b_ ]',
-        data: ''
-      },
-      {
-        label: 'Contenido de agregado en la mezcla (%) [P _s_ ]',
-        data: ''
-      }
-    ]
+    wbi: '',
+    wbf: '',
+    wp: ''
   }),
 
   computed: {
-    ...mapState('contents', ['pb', 'pb_', 'ps']),
-    wp() {
-      return this.fields[0].data
-    },
-    wbi() {
-      return this.fields[1].data
-    },
-    wbf() {
-      return this.fields[2].data
-    }
+    ...mapGetters('amaac', ['pb', 'pb_', 'ps'])
   },
 
   methods: {
-    ...mapMutations('contents', ['updatePb_', 'updatePb', 'updatePs']),
+    ...mapMutations('amaac', ['updatePb_', 'updatePb', 'updatePs']),
     calculatePb() {
       const pb = this.asphaltByMixture(this.pb_)
       this.updatePb({ value: pb })
-      this.results[1].data = this.pb
     },
     calculatePb_() {
       const pb_ = this.asphaltByAggregates(this.wbi, this.wbf, this.wp)
       this.updatePb_({ value: pb_ })
-      this.results[0].data = this.pb_
     },
     calculatePs() {
       const ps = this.aggregatesByMixture(this.pb)
       this.updatePs({ value: ps })
-      this.results[2].data = this.ps
     },
     calculateAll() {
       this.calculatePb_()
