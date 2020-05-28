80f73ed2dd3a354f42d0d399aaa8b82a832c2406
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-28 10:21:55 -0500

    Create 'results' component
    Add also its utility for automated generated fields.

---
+++ src/utils/results.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    label: "Contenido de asfalto por agregado seco (%) [P _b_ ']",
+    data: ''
+  },
+  {
+    label: 'Contenido de asfalto en la mezcla (%) [P _b_ ]',
+    data: ''
+  },
+  {
+    label: 'Contenido de agregado en la mezcla (%) [P _s_ ]',
+    data: ''
+  },
+  {
+    label: 'Absorción del agrgado (%)',
+    data: ''
+  },
+  {
+    label: 'Densidad relativa (seca) bruta del agregado [G _sb_ ]',
+    data: ''
+  },
+  {
+    label: 'Densidad relativa aparente del agregado [G _sa_ ]',
+    data: ''
+  },
+  {
+    label: 'Densidad relativa del asfalto [G _b_ ]',
+    data: ''
+  },
+  {
+    label: 'Densidad relativa bruta de la mezcla compacta [G _mb_ ]',
+    data: ''
+  }
+]
---
+++ src/views/calculators/amaac/Results.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <p class="title">Resultados</p>
+    <form>
+      <app-field
+        :key="r"
+        :label="result.label"
+        :readonly="true"
+        :value="result.data"
+        v-for="(result, r) in results"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+import AppField from '@/components/AppField.vue'
+import results from '@/utils/results'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Results',
+
+  components: { AppField },
+
+  data: () => ({
+    results
+  }),
+
+  computed: {
+    ...mapGetters('amaac', ['abs', 'gsa', 'gsb', 'pb', 'pb_', 'ps'])
+  },
+
+  watch: {
+    abs(value) {
+      this.results[3].data = value
+    },
+    gsa(value) {
+      this.results[5].data = value
+    },
+    gsb(value) {
+      this.results[4].data = value
+    },
+    pb(value) {
+      this.results[1].data = value
+    },
+    pb_(value) {
+      this.results[0].data = value
+    },
+    ps(value) {
+      this.results[2].data = value
+    }
+  }
+}
+</script>
