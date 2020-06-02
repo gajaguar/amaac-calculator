c3c3159bc19633b8de7177df70aa1e88d3aa2246
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-06-02 08:41:11 -0500

    Refactor the way of display the results

---
+++ src/views/calculators/amaac/Derived.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <p class="title">Resultados Derivados</p>
+    <form>
+      <app-field
+        :readonly="true"
+        :value="gse"
+        label="Densidad efectiva del agregado [G _se_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="pba"
+        label="Procentaje de asfalto absorbido [P _ba_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="pbe"
+        label="Procentaje de asfalto efectivo [P _be_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="vma"
+        label="Vacíos en el agrgado [VMA]"
+      />
+      <app-field
+        :readonly="true"
+        :value="pa"
+        label="Vacíos en la mezcla compacta [P _a_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="vfa"
+        label="Vacíos llenados por el asfalto [VFA]"
+      />
+      <app-field
+        :readonly="true"
+        :value="fb"
+        label="Relación Polvo - Asfalto [F/B]"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+import AppField from '@/components/AppField.vue'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Derived',
+
+  components: { AppField },
+
+  computed: {
+    ...mapGetters('amaac', ['fb', 'gse', 'pa', 'pba', 'pbe', 'vfa', 'vma'])
+  }
+}
+</script>
---
+++ src/views/calculators/amaac/Direct.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <p class="title">Resultados Directos</p>
+    <form>
+      <app-field
+        :readonly="true"
+        :value="pb_"
+        label="Contenido de asfalto por agregado seco (%) [P _b_ ']"
+      />
+      <app-field
+        :readonly="true"
+        :value="pb"
+        label="Contenido de asfalto en la mezcla (%) [P _b_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="ps"
+        label="Contenido de agregado en la mezcla (%) [P _s_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="abs"
+        label="Absorción del agrgado (%)"
+      />
+      <app-field
+        :readonly="true"
+        :value="gsb"
+        label="Densidad relativa (seca) bruta del agregado [G _sb_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="gsa"
+        label="Densidad relativa aparente del agregado [G _sa_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="gb"
+        label="Densidad relativa del asfalto [G _b_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="gmb"
+        label="Densidad relativa bruta de la mezcla compacta [G _mb_ ]"
+      />
+      <app-field
+        :readonly="true"
+        :value="gmm"
+        label="Densidad relativa Máxima de la mezcla [G _mm_ ]"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+import AppField from '@/components/AppField.vue'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Direct',
+
+  components: { AppField },
+
+  computed: {
+    ...mapGetters('amaac', [
+      'abs',
+      'gb',
+      'gmb',
+      'gmm',
+      'gsa',
+      'gsb',
+      'pb',
+      'pb_',
+      'ps'
+    ])
+  }
+}
+</script>
